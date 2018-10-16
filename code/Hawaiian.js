961a44d0de9226da24b26cfbed18afd481fb374d
Becki Wordsworth <r.a.wordsworth@gmail.com>
2018-10-16 11:16:19 +0200

    Stretch goal added

--- code/Hawaiian.js
+++ code/Hawaiian.js
@@ -1,11 +1,15 @@
-let Hawaiian = 'Hawaiian pizza is a pizza topped with tomato sauce, cheese, pineapple, and back bacon or ham. Some versions may include peppers, mushrooms, bacon or pepperoni.[citation needed]  \
+let Hawaiian = 'Hawaiian pizza is a pizza topped with tomato sauce, cheese, pineapple, and back bacon or ham. Some versions may include peppers, mushrooms, bacon or pepperoni. Pineapple as a pizza topping divides public opinion: Hawaiian was the most popular pizza in Australia in 1999, accounting for 15% of pizza sales,[1] and a 2015 review of independent UK takeaways operating through Just Eat found the Hawaiian pizza to be the most commonly available. However, a 2016 survey of US adults had pineapple in the top three least favourite pizza toppings, ahead of anchovies and mushrooms. Greek-Canadian Sam Panopoulos claimed that he created the first Hawaiian pizza at the Satellite Restaurant in Chatham, Ontario, Canada in 1962. Inspired in part by his experience preparing Chinese dishes which commonly mix sweet and savoury flavours, Panopoulos experimented with adding pineapple, ham, bacon and other toppings which were not initially very popular.'
 
- Pineapple as a pizza topping divides public opinion: Hawaiian was the most popular pizza in Australia in 1999, accounting for 15% of pizza sales,[1] and a 2015 review of independent UK takeaways operating through Just Eat found the Hawaiian pizza to be the most commonly available.[2] However, a 2016 survey of US adults had pineapple in the top three least favourite pizza toppings, ahead of anchovies and mushrooms.[3] \
 
- Greek-Canadian Sam Panopoulos claimed that he created the first Hawaiian pizza at the Satellite Restaurant in Chatham, Ontario, Canada in 1962. Inspired in part by his experience preparing Chinese dishes which commonly mix sweet and savoury flavours, Panopoulos experimented with adding pineapple, ham, bacon and other toppings which were not initially very popular.[4][5][6][7][8] The addition of pineapple to the traditional mix of tomato sauce and cheese, sometimes with ham or sometimes with \ bacon, soon became popular locally and eventually became a staple offering of pizzerias around the world.[9] \
-
- In Germany, Hawaiian pizza is thought to be a variation of the ham, pineapple and cheese topped Toast Hawaii, originally introduced by Germany\'s first TV cook Clemens Wilmenrod in 1955.[10][11][12]'
-
-const countPizza = (pizza) => {
-  return pizza.count()
+ const hawaiianPizza = (words) => {
+   pizzaWords = words.split(" ")
+   return pizzaWords.length
  }
+
+ const pineappleCounter = (words) => {
+  pineapple = words.match(/pineapple/gi)
+  return pineapple.length
+}
+
+console.log("Words: " + hawaiianPizza(Hawaiian))
+console.log("Number of times the word pineapple appears: " + pineappleCounter(Hawaiian))
